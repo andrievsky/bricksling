94e606b128f6c1a992ba2579941046214f39120b
Mykola Andriievskyi <mykola.andriievskyi@sap.com>
2025-05-16 22:46:14 +0200

    add new creations

--- main.go
+++ main.go
@@ -10,12 +10,15 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"slices"
 
 	"image"
 	"image/jpeg"
 
 	"github.com/nfnt/resize"
 )
+
+var addImagesFlag = flag.Bool("add-images", false, "Add images")
 
 func main() {
 	serveFlag := flag.Bool("serve", false, "Start the server")
@@ -99,16 +102,53 @@ func build() {
 	}
 	defer outputFile.Close()
 
+	// Create the images output directory if it doesn't exist
+	if _, err := os.Stat(imagesOutputDir); os.IsNotExist(err) {
+		os.MkdirAll(imagesOutputDir, os.ModePerm)
+	}
+
+	// Find unused images
+	unusedImages, err := findUnusedImages(postsData, imagesPath)
+	if err != nil {
+		fmt.Printf("Error finding unused images: %v\n", err)
+		return
+	}
+
+	if len(unusedImages) > 0 {
+		if *addImagesFlag {
+			fmt.Println("Adding unused images to the index json...")
+			slices.Reverse(unusedImages)
+			newPosts := make([]Post, 0)
+			for _, image := range unusedImages {
+				fmt.Printf("Adding image: %s\n", image)
+				newPosts = append(newPosts, Post{
+					Title:   "New",
+					Caption: "Meaningful caption",
+					Image:   image,
+				})
+			}
+			postsData.Posts = append(newPosts, postsData.Posts...)
+			postsDataJSON, err := json.MarshalIndent(postsData, "", "  ")
+			if err != nil {
+				fmt.Printf("Error marshalling updated JSON data: %v\n", err)
+				return
+			}
+			err = os.WriteFile(indexJSONPath, postsDataJSON, 0644)
+			if err != nil {
+				fmt.Printf("Error writing updated JSON data to file: %v\n", err)
+				return
+			}
+			fmt.Println("Updated index.json with new images.")
+		} else {
+			fmt.Println("Unused images found, use -add-images flag to add them to the index json.")
+		}
+	}
+
 	// Execute template with the data
 	err = tmpl.Execute(outputFile, postsData)
 	if err != nil {
 		fmt.Printf("Error executing template: %v\n", err)
 		return
-	}
-
-	// Create the images output directory if it doesn't exist
-	if _, err := os.Stat(imagesOutputDir); os.IsNotExist(err) {
-		os.MkdirAll(imagesOutputDir, os.ModePerm)
 	}
 
 	// Copy and resize images
@@ -156,4 +196,31 @@ func build() {
 	}
 
 	fmt.Println("HTML and images have been generated successfully.")
+}
+
+func findUnusedImages(postsData PostsData, imagesPath string) ([]string, error) {
+	usedImages := make(map[string]bool)
+	for _, post := range postsData.Posts {
+		usedImages[post.Image] = true
+	}
+
+	var unusedImages []string
+	err := filepath.Walk(imagesPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && (filepath.Ext(path) == ".jpg" || filepath.Ext(path) == ".jpeg") {
+			imageName := filepath.Base(path)
+			if !usedImages[imageName] {
+				unusedImages = append(unusedImages, imageName)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return unusedImages, nil
 }
